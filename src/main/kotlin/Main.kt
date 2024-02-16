5956293b047c6167d44d70db166ac5c6628f6882
Kim <nicedarrow12@gmail.com>
2024-02-15 21:03:32 -0300

    null safety

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,31 @@
-fun main(args: Array<String>) {
-    var isApproved = true
-    var firstTime: Boolean = false
+fun main() {
+    // Tipo Nullable: NullSafety
+    var idade: Int? = null // A interrogação permite que a variável idade use o valor null
+    println(idade)
 
-    val (address,number) = Pair("Av. Lins de Vasconcelos", 1264)
-    println(address)
-    println(number)
+    idade = 20
+    println(idade)
+
+    // Exemplo =================
+
+    // A atribuição de null a um var também não será compilada
+    //var driverLicense: String = "6789877"
+    //driverLicense = null // não compila
+    // Para que uma variável contenha um valor null é necessário
+    // o uso do sufixo ? no tipo
+
+    /* var driverLicense: String? = null
+    println(driverLicense)
+    driverLicense = "6789877"
+    println(driverLicense)
+    driverLicense = null // agora compila
+    println(driverLicense) */
+
+    var driverLicense: String? = null
+    // driverLicense = "6789877"
+    if (driverLicense != null) {
+        println("A carteira de motorista é $driverLicense")
+    } else {
+        println("Esta pessoa não possui carteira de motorista")
+    }
 }
