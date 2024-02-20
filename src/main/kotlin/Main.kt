8bdd9cdb971776d055bd453b2c30c144a4968e3b
Kim <nicedarrow12@gmail.com>
2024-02-20 20:13:27 -0300

    Collections in Kotlin: Set

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,51 +1,31 @@
 fun main() {
-    //Criando um List de Strings
-    var movies = ArrayList<String> ()
-    movies.addAll(listOf(
-        "Matrix",
-        "Vingadores",
-        "Jurassic Park",
-        "De Volta para o Futuro"
-    ))
-//Criando um list vazio
-    var movies2 = ArrayList<String> ()
-//Inserindo elementos
-    movies.add("Homem-Aranha: De Volta ao Lar")
-    println(movies.count()) //5
+    // Set não permite itens repetidos
+    var filmes = HashSet<String>()
+    println(filmes.size)
+
+    filmes.add("Homem-Aranha 1")
+    filmes.add("Homem-Aranha 2")
+    filmes.add("Homem-Aranha 3")
+    println(filmes.size)
+
+    println(filmes)
+
+    filmes.add("Homem-Aranha 1")
+    println(filmes)
+
+    filmes.add("Homem-Aranha 4")
+    println(filmes)
+
+    println(filmes.contains("Homem-Aranha 1"))
+    println(filmes.contains("Homem Aranha 1"))
+
+    filmes.remove("Homem-Aranha 4")
+    println(filmes)
+
     println("\n")
-//Perceba que o código abaixo vai alterar a quantidade
-//de itens do List pois ele aceita itens repetidos.
-    movies.add("Homem-Aranha: De Volta ao Lar")
-    println(movies)   //[Matrix, Vingadores, Jurassic Park, De Volta ao Futuro, Homem-Aranha: De Volta ao Lar, Homem-Aranha: De Volta ao Lar]
-    println(movies.count()) //6 (2 elementos repetidos)
-    println("\n")
-//Removendo 2 elementos repetidos
-    movies.remove("Homem-Aranha: De Volta ao Lar")
-    movies.remove("Homem-Aranha: De Volta ao Lar")
-    println(movies)   //["Vingadores", "De Volta para o Futuro", "Matrix", "Jurassic Park"]
-    println("\n")
-//Percorrendo um List
-    for (movie in movies) {
-        println(movie)
-    }
-    println("\n")
-//Verificando se determinado elemento está contido no List
-    if (movies.contains("Matrix")) {
-        println("Matrix está na minha lista de filmes favoritos!!")
-    }
-    println("\n")
-//Vamos criar um novo List para realizarmos algumas operações
-//No exemplo abaixo, usaremos um formato mais simplificado de criação de List
-    var myWifeMovies  = listOf(
-        "De Repente 30",
-        "Mensagem para você",
-        "Sintonia de Amor",
-        "De Volta para o Futuro",
-        "Jurassic Park"
-    )
-//Criando um List com todos os filmes
-    var allMovies = movies + myWifeMovies
-    println(allMovies)
-//[Matrix, Vingadores, Jurassic Park, De Volta para o Futuro, De Repente 30, Mensagem para você, Sintonia de Amor, De Volta para o Futuro, Jurassic Park]
-    println("\n")
+    // ===========================
+    // Forma reduzida:
+    var precos = setOf(45.9, 78.7, 54.9, 41.9)
+    println(precos)
+    // QUando criado dessa forma, não é possível adicionar novos elementos
 }
